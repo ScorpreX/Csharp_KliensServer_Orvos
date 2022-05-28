b3afbdcd95d050559adc3d88cf4cc06c6273acf0
Vferi98 <vassferenc4@gmail.com>
2022-05-28 19:56:23 +0200

    initial commit

---
+++ WebApi_Common/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi_Common.Models
+{
+    public class Patient
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string Symptom { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Symptom}";
+        }
+    }
+}

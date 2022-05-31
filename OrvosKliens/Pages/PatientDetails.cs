04eff93d4c1394429e30b76a85ae52276b0416f2
norbert204 <norbert204@mailbox.unideb.hu>
2022-05-31 16:04:22 +0200

    Páciens részletes adatok/módosítás oldal

---
+++ OrvosKliens/Pages/PatientDetails.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebApi_Common.Models;
+
+namespace OrvosKliens.Pages
+{
+    public partial class PatientDetails
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+        public Patient Patient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Patient = await HttpClient.GetFromJsonAsync<Patient>(HttpClient.BaseAddress + $"/patient/{Id}");
+            await base.OnInitializedAsync();
+        }
+
+        private async Task ModifyPatient()
+        {
+            await HttpClient.PutAsJsonAsync<Patient>(HttpClient.BaseAddress + $"/patient", Patient);
+            NavigationManager.NavigateTo("/patients");
+        }
+    }
+}

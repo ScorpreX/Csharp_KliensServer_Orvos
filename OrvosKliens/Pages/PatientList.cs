00909203afa795f26e185c0cb2c91aec22cbf5c7
norbert204 <norbert204@mailbox.unideb.hu>
2022-06-05 18:49:38 +0200

    Refaktor: PatientList.cs

--- OrvosKliens/Pages/PatientList.cs
+++ OrvosKliens/Pages/PatientList.cs
@@ -8,15 +8,20 @@ namespace OrvosKliens.Pages
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
-        private Patient[] _patients { get; set; }
+
         public Patient[] PatientsToList { get; set; }
 
+        private Patient[] _patients;
+
         private string _search { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             _patients = await HttpClient.GetFromJsonAsync<Patient[]>("patient");
             _patients = _patients.OrderBy(p => p.RecordTime).ToArray();
+
             UpdatePatientsToList(null);
+            
             await base.OnInitializedAsync();   
         }
 
@@ -31,7 +36,6 @@ namespace OrvosKliens.Pages
                 _search = "";
             }
             PatientsToList = _patients.Where(p => p.Name.Contains(_search)).ToArray();
-            //PatientsToList = _patients;
         }
     }
 }
